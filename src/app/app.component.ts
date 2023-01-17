b503b6afc482bcfe6d333d7da5a0697c3f583b27
KacperBond <56804274+KacperBond@users.noreply.github.com>
2023-01-17 13:19:20 +0100

    another empty index test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Dynamic Widgets</h1> Hello World Template!'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ds-cloud-widget';
-
-
-
-  test(){
-    alert("TEST ALERT");
-  }
 }
